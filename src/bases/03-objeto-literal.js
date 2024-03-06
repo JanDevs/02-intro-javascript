f5e0fccc98e3f57dac786eb6771442ddf16a041e
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2024-03-06 13:15:05 -0600

    Objeto y literales

---
+++ src/bases/03-objeto-literal.js
@@ -0,0 +1,19 @@
+const persona = {
+    nombre: 'Tony',
+    apellido: 'Stark',
+    edad: 45,
+    direccion: {
+        ciudad: 'New York',
+        zip: 52525252,
+        lat: 14.3232,
+        lng: 34.4343,
+    },
+};
+
+console.log( persona );
+
+const persona2 = { ...persona };
+persona2.nombre = 'Peter';
+
+console.log( persona );
+console.log( persona2 );
