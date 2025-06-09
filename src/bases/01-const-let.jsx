0ea578e0373369360ade85e914734648f0a9db56
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-06-09 13:11:45 -0600

    Variables y constantes

---
+++ src/bases/01-const-let.jsx
@@ -0,0 +1,16 @@
+//Variables y constantes
+const nombre = "Jan";
+const apellido = "Juarez";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    let valorDado = 6;
+
+    console.log(valorDado);
+}
+
+console.log(valorDado);
